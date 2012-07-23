b8dbd91b06a438777219ed62dcc60b851f50245d
Jonathan Swartz <swartz@pobox.com>
2012-07-23 05:38:49 -0700

    init

---
+++ lib/Code/TidyAll/Plugin/Mason/IgnoreDollarDotFilter.pm
@@ -0,0 +1,43 @@
+package Code::TidyAll::Plugin::Mason::IgnoreDollarDotFilter;
+use strict;
+use warnings;
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Code::TidyAll::Plugin::Mason::IgnoreDollarDotFilter - Ignore $. in Mason
+components
+
+=head1 SYNOPSIS
+
+    use Code::TidyAll::Plugin::Mason::IgnoreDollarDotFilter;
+
+=head1 DESCRIPTION
+
+
+
+=head1 SUPPORT AND DOCUMENTATION
+
+Questions and feedback are welcome, and should be directed to the mailing list:
+
+    http://groups.google.com/...
+
+Bugs and feature requests will be tracked at RT:
+
+    http://rt.cpan.org/NoAuth/Bugs.html?Dist=Code-TidyAll-Plugin-Mason-IgnoreDollarDotFilter
+    bug-code-tidyall-plugin-mason-ignoredollardotfilter@rt.cpan.org
+
+The latest source code can be browsed and fetched at:
+
+    http://github.com/jonswar/perl-code-tidyall-plugin-mason-ignoredollardotfilter
+    git clone git://github.com/jonswar/perl-code-tidyall-plugin-mason-ignoredollardotfilter.git
+
+=head1 SEE ALSO
+
+L<Some::Module>
+
